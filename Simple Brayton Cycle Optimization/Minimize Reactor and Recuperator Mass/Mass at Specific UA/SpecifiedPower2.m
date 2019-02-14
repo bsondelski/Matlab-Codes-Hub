88ba829afc60cee6686919426499da42df104562
Unknown <sondelski@wisc.edu>
2019-02-14 13:12:26 -0600

    2/14 integrated specifiedPowerError into specifiedPower2 - works

--- Simple Brayton Cycle Optimization/Minimize Reactor and Recuperator Mass/Mass at Specific UA/SpecifiedPower2.m
+++ Simple Brayton Cycle Optimization/Minimize Reactor and Recuperator Mass/Mass at Specific UA/SpecifiedPower2.m
@@ -130,6 +130,33 @@ if a == 1
     T2 = NaN;
     p2 = NaN;
 end
+
+function [err] = specifiedPowerError(m_dot_guess,power,p1,T4,PR_c,UA,...
+    A_panel,T_amb,fluid,mode)
+% find error in specified power for the guessed mass flow rate
+
+% Inputs:
+% m_dot_guess: the guessed mass flow in the cycle [kg/s]
+% power: specified power for the system 
+% p1: flow pressure at inlet of the compressor [kPa] 
+% T4: Temp at turbine inlet [K]
+% PR_c: pressure ratio of the compressor
+% UA: conductance of recuperator [W/K]
+% A_panel: area of radiator panel [m2]
+% T_amb: ambient temp for radiator [K]
+% fluid: working fluid for the system
+% Mode: 1(constant property model),2(use of FIT),3(use of REFPROP)
+
+% Output:
+% err: error between specified power and power from guessed mass flow rate
+
+[cyclePower,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~,~] = ...
+    BraytonCycle(m_dot_guess,p1,T4,PR_c,UA,A_panel,T_amb,fluid,mode,0);
+
+err = power-cyclePower;
+
+
+end
     
 end
 
